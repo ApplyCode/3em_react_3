2b35fddbf5aa55b9409ccbde43b3b1e07b940223
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-29 22:21:05 -0400

    Update Loader.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,4 @@
-// material-ui
-import { styled } from '@mui/material/styles';
-import LinearProgress from '@mui/material/LinearProgress';
+
 
 // loader style
 const LoaderWrapper = styled('div')(({ theme }) => ({
