73384ca612d34b0c3ea98595dcd9254cdc5624e5
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-29 22:23:30 -0400

    Update index.js

--- src/layout/CommonLayout/index.js
+++ src/layout/CommonLayout/index.js
@@ -19,16 +19,7 @@ const FooterBlock = lazy(() => import('./FooterBlock'));
 
 // ==============================|| Loader ||============================== //
 
-const LoaderWrapper = styled('div')(({ theme }) => ({
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  zIndex: 2001,
-  width: '100%',
-  '& > * + *': {
-    marginTop: theme.spacing(2)
-  }
-}));
+
 
 const Loader = () => (
   <LoaderWrapper>
