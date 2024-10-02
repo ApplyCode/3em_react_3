0e949a8953687918aa0a53ecb0f1f5552914e0cd
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-10-02 03:33:41 -0400

    Update engineering.js

--- src/api/engineering.js
+++ src/api/engineering.js
@@ -2,7 +2,11 @@ import config from '../config';
 
 export async function getEngineeringSite(pageIndex, pageSize) {
   const page = pageIndex + 1
-
-
+  const response = await fetch(config.api_url+`/engineering/site?paginate=true&page=${encodeURIComponent(page)}&pageSize=${encodeURIComponent(pageSize)}`, {
+    method: 'GET',
+    headers: new Headers({
+      'Authorization': 'Bearer '+localStorage.getItem('settings')
+    }),
+  })
   return await response.json();
 }
