a625526b0a0a26ba5842f36b3ef64c0cc5209aae
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-10-13 09:37:22 -0400

    Update ExpenseTable.js

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -1,7 +1,4 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
+
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import SaveIcon from '@mui/icons-material/Save';
