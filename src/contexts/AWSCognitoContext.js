ac8b34566157c970a2d813911fc2c903afc66ebe
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-10-19 12:08:00 -0400

    Update AWSCognitoContext.js

--- src/contexts/AWSCognitoContext.js
+++ src/contexts/AWSCognitoContext.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
 import { createContext, useEffect, useReducer } from 'react';
 
-
+// third-party
+import { CognitoUser, CognitoUserPool, CognitoUserAttribute, AuthenticationDetails } from 'amazon-cognito-identity-js';
 
 // action - state management
 import { LOGIN, LOGOUT } from 'store/reducers/actions';
