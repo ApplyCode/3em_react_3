0387eb678fa8f50aa9a4024f8dad677732c37c72
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-10-22 22:23:41 -0400

    Update ScrollTop.js

--- src/components/ScrollTop.js
+++ src/components/ScrollTop.js
@@ -8,7 +8,13 @@ const ScrollTop = ({ children }) => {
   const location = useLocation();
   const { pathname } = location;
 
-
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }, [pathname]);
 
   return children || null;
 };
