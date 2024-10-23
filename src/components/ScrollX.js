986cdedd808c4149b3bc17a4ee1d1b848bd79750
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-10-22 22:22:42 -0400

    Update ScrollX.js

--- src/components/ScrollX.js
+++ src/components/ScrollX.js
@@ -1,10 +1,1 @@
-// material-ui
-import { styled } from '@mui/material/styles';
 
-const ScrollX = styled('div')({
-  width: '100%',
-  overflowX: 'auto',
-  display: 'block'
-});
-
-export default ScrollX;
